28b4a2138393befa8d692fab8e50f744c75a1e6b
christopherrons <christopherheron09@gmail.com>
2022-12-29 11:00:59 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.1"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.herron.exchange"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_HIGHER
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/herron/exchange/common/api/CommonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herron.exchange.common.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
