ad9178671a33355fbdc40f83045c435fc7a195d8
christopherrons <christopherheron09@gmail.com>
2023-01-01 04:43:22 +0100

    update partition key

--- common/src/main/java/com/herron/exchange/common/api/common/model/PartitionKey.java
+++ common/src/main/java/com/herron/exchange/common/api/common/model/PartitionKey.java
@@ -3,4 +3,8 @@ package com.herron.exchange.common.api.common.model;
 import com.herron.exchange.common.api.common.enums.TopicEnum;
 
 public record PartitionKey(TopicEnum topicEnum, int partitionId) {
+
+    public String description() {
+        return topicEnum.getTopicName() + "-" + partitionId;
+    }
 }
