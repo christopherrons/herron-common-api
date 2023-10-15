1eca6ed08643acb8610f40b00c81bcab7f13cc0a
christopherrons <christopherheron09@gmail.com>
2023-10-15 23:46:10 +0200

    Refactoring

--- common/src/main/java/com/herron/exchange/common/api/common/enums/KafkaTopicEnum.java
+++ common/src/main/java/com/herron/exchange/common/api/common/enums/KafkaTopicEnum.java
@@ -1,10 +1,10 @@
 package com.herron.exchange.common.api.common.enums;
 
 public enum KafkaTopicEnum {
-    HERRON_ORDER_DATA("herron-order-data"),
+    ORDER_DATA("order-data"),
     PREVIOUS_SETTLEMENT_PRICE_DATA("previous-settlement-price-data"),
-    HERRON_REFERENCE_DATA("herron-reference-data"),
-    HERRON_AUDIT_TRAIL("herron-audit-trail");
+    REFERENCE_DATA("reference-data"),
+    AUDIT_TRAIL("audit-trail");
 
     private final String topicName;
 
