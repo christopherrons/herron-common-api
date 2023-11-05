6e35b4604b2c687fba5fd21ced161d29bd6c481c
christopherrons <christopherheron09@gmail.com>
2023-11-05 15:17:58 +0100

    Dependency updates.

--- common/src/main/java/com/herron/exchange/common/api/common/parametricmodels/forwardcurve/ForwardPriceCurve.java
+++ common/src/main/java/com/herron/exchange/common/api/common/parametricmodels/forwardcurve/ForwardPriceCurve.java
@@ -7,7 +7,6 @@ import com.herron.exchange.common.api.common.api.math.Function2d;
 import com.herron.exchange.common.api.common.math.interpolation.ConstantIntervalCurve;
 import com.herron.exchange.common.api.common.math.interpolation.CubicSplineInterpolation;
 import com.herron.exchange.common.api.common.math.interpolation.LinearInterpolationCurve;
-import com.herron.exchange.common.api.common.messages.common.PureNumber;
 import com.herron.exchange.common.api.common.parametricmodels.forwardcurve.model.ForwardCurveModelParameters;
 
 public class ForwardPriceCurve {
--- common/src/main/java/com/herron/exchange/common/api/common/parametricmodels/impliedvolsurface/ImpliedVolatilitySurface.java
+++ common/src/main/java/com/herron/exchange/common/api/common/parametricmodels/impliedvolsurface/ImpliedVolatilitySurface.java
@@ -1,30 +1,34 @@
 package com.herron.exchange.common.api.common.parametricmodels.impliedvolsurface;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.herron.exchange.common.api.common.enums.OptionTypeEnum;
+
+import static com.herron.exchange.common.api.common.enums.OptionTypeEnum.CALL;
 
 public class ImpliedVolatilitySurface {
 
     private final String id;
-    private final double value;
+    private final double underlyingPrice;
 
-    public ImpliedVolatilitySurface(@JsonProperty("id") String id, @JsonProperty("value") double value) {
+    public ImpliedVolatilitySurface(@JsonProperty("id") String id, @JsonProperty("underlyingPrice") double underlyingPrice) {
         this.id = id;
-        this.value = value;
+        this.underlyingPrice = underlyingPrice;
     }
 
     public static ImpliedVolatilitySurface create(String id, double value) {
         return new ImpliedVolatilitySurface(id, value);
     }
 
-    public double getImpliedVolatility(double timeToMaturity, double logMoneyness) {
-        return value;
+    public double getImpliedVolatility(double timeToMaturity, double strikePrice, OptionTypeEnum optionType) {
+        double logMoneyness = optionType == CALL ? Math.log(underlyingPrice / strikePrice) : Math.log(strikePrice / underlyingPrice);
+        return 0.0;
     }
 
     public String getId() {
         return id;
     }
 
-    public double getValue() {
-        return value;
+    public double getSpotPrice() {
+        return underlyingPrice;
     }
 }
