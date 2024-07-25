4313e154660424a7cc526cfe9d27d4a6e705b871
christopherrons <christopherheron09@gmail.com>
2024-07-25 15:27:40 +0200

    Update to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ allprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_21
     }
 
     publishing {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,9 @@ include("common")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.0.1")
-            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.0.1")
-            library("spring.kafka", "org.springframework.kafka:spring-kafka:3.0.1")
+            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.2.0")
+            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.2.0")
+            library("spring.kafka", "org.springframework.kafka:spring-kafka:3.2.0")
             library("commons.math", "org.apache.commons:commons-math3:3.2")
             library("immutables.value", "org.immutables:value:2.9.0")
             library("immutables.builder", "org.immutables:builder:2.9.0")
@@ -15,10 +15,10 @@ dependencyResolutionManagement {
         }
 
         create("testlibs") {
-            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.0.1")
+            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.2.0")
             library("junit.jupiter.api", "org.junit.jupiter:junit-jupiter-api:5.8.1")
             library("junit.jupiter.engine", "org.junit.jupiter:junit-jupiter-engine:5.8.1")
-            library("spring.kafka.test", "org.springframework.kafka:spring-kafka:3.0.1")
+            library("spring.kafka.test", "org.springframework.kafka:spring-kafka:3.2.0")
             library("podam.random", "uk.co.jemos.podam:podam:7.1.0.RELEASE")
         }
     }
